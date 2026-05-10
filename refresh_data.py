40af7df0609574b6550ee55d910d6f4b3176bee1
abhishek31011983 <abhishek.gupta1985@gmail.com>
2026-05-10 11:30:03 +0530

    Update refresh_data.py

--- refresh_data.py
+++ refresh_data.py
@@ -129,7 +129,7 @@ if len(account_raw) > 1:
                 "date":                row[0],
                 "nifty_close":         row[1] if len(row) > 1 else "",
                 "deposit_withdrawal":  row[2] if len(row) > 2 else "",
-                "portfolio_size":      row[3] if len(row) > 3 else ""
+                "portfolio_size":      row[3] if len(row) > 3 else "",
                 "weekly_change":      row[4] if len(row) > 4 else "",
                 "mtf_loan":           row[7] if len(row) > 7 else "",
                 "net_equity":         row[8] if len(row) > 8 else "",
